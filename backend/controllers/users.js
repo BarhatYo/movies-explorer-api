222d339e6dbee9cee8be8ce6f52462d40fcb01ea
Sergey Barkhatov <sergeybarkhat@yandex.ru>
2024-01-08 11:09:49 +0100

    fix: обновление юзера при конфликте

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,18 +1,18 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const User = require('../models/user');
-const { CREATED, SUCCESS } = require('../constants/statusCodes');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const { CREATED, SUCCESS } = require("../constants/statusCodes");
 const {
   USER_NOT_FOUND_MESSAGE,
   USER_CONFLICT_MESSAGE,
   USER_BAD_REQUEST_MESSAGE,
-} = require('../constants/errorMessages');
+} = require("../constants/errorMessages");
 
-const BadRequest = require('../utils/BadRequest');
-const Conflict = require('../utils/ConflictError');
-const NotFound = require('../utils/NotFound');
+const BadRequest = require("../utils/BadRequest");
+const Conflict = require("../utils/ConflictError");
+const NotFound = require("../utils/NotFound");
 
 const getProfile = (req, res, next) => {
   const userId = req.user._id;
@@ -22,28 +22,27 @@ const getProfile = (req, res, next) => {
       if (!user) {
         throw new NotFound(USER_NOT_FOUND_MESSAGE);
       }
-      res.send(
-        {
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-        },
-      );
+      res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
     })
     .catch(next);
 };
 
 const createUser = (req, res, next) => {
-  const {
-    email, password, name,
-  } = req.body;
+  const { email, password, name } = req.body;
 
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      email,
-      password: hash,
-      name,
-    }))
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        email,
+        password: hash,
+        name,
+      })
+    )
     .then((user) => {
       res.status(CREATED).send({
         _id: user._id,
@@ -63,19 +62,26 @@ const updateUser = (req, res, next) => {
   const userId = req.user._id;
   const { name, email } = req.body;
 
-  User.findByIdAndUpdate(userId, { name, email }, {
-    new: true,
-    runValidators: true,
-  })
-    .then((user) => res.send(
-      {
+  User.findByIdAndUpdate(
+    userId,
+    { name, email },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .then((user) =>
+      res.send({
         _id: user._id,
         name: user.name,
         email: user.email,
-      },
-    ))
+      })
+    )
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
+        return next(new BadRequest(USER_BAD_REQUEST_MESSAGE));
+      }
+      if (err.name === "ValidationError") {
         return next(new BadRequest(USER_BAD_REQUEST_MESSAGE));
       }
       return next(err);
@@ -88,8 +94,8 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-        { expiresIn: '7d' },
+        NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+        { expiresIn: "7d" }
       );
       res.status(SUCCESS).send({ token });
     })
